eeffdc8f0856ba7cc48501b5f380e6254e99c637
arturo-blanco <arturoblanco@tdf.com>
2022-10-27 18:05:25 -0300

    se crea slot progresivo

--- class/progressiveSlot.ts
+++ class/progressiveSlot.ts
@@ -1,23 +1,54 @@
 import { Slot } from "./slot";
 export class ProgressiveSlot extends Slot {
-    private payLine:number[];
-    private jackpot:number;
+  private payLine: number;
+  private jackpot: number;
 
-    public constructor(pId:number,pBetValue:number[],pTheme:string,pSymbolsNumber:number,pWinProbability:number,pPayLine:number[],pJackpot:number) {
-        super(pId,pBetValue,pTheme,pSymbolsNumber,pWinProbability);
-        this.payLine=pPayLine;
-        this.jackpot=pJackpot;
+  public constructor(pId:number,pBetValue:number[],pTheme:string,pSymbolsNumber:number,pWinProbability:number,
+pRollerNumber:number,pPayLine:number,pJackpot:number){
+    super(pId,pBetValue,pTheme,pSymbolsNumber,pWinProbability,pRollerNumber);
+    this.payLine = pPayLine;
+    this.jackpot = pJackpot;
+  }
+  public getPayLine():number{
+    return this.payLine;
+  }
+  public getJackpot():number{
+    return this.jackpot;
+  }
+  public setPayLine(newPayLine:number):void{
+    this.payLine=newPayLine;
+  }
+  public setJackpot(newJackpot:number):void{
+    this.jackpot=newJackpot;
+  }
+  private progressiveCombination():number{
+    let aux=new Array(this.payLine);
+    let combination=0;
+      for(let i:number=0;i<aux.length;i++){
+        combination+=aux[i]=this.getReward();
+      } return combination;
     }
-    public getPayLine():number[]{
-        return this.payLine;
-    }
-    public getJackpot():number{
-        return this.jackpot;
-    }
-    public setPayLine(newPayLine:number[]):void{
-        this.payLine=newPayLine;
-    }
-    public setJackpot(newJackpot:number):void{
-        this.jackpot=newJackpot;
-    }
-}
+    public playprogressiveSlot(pBetValue:number):void{
+      let reward=0;
+        if(this.verifyBet(pBetValue) && this.checkRollers()){
+            reward=this.progressiveCombination()
+        }
+          if(reward===-1){
+              console.log(`Felicidades acertó una linea de 7! Ganó ${pBetValue*500}`);
+          } else if(reward===-2){
+              console.log(`Felicidades acertó dos lineas de 7! Ganó ${pBetValue*1000}`);
+          } else if(reward===-3){
+              console.log(`¡¡¡Felicidades acertó el jackpot!!! Ganó ${this.getJackpot()}`);
+          } 
+          else if(reward===0){
+            console.log(`Suerte para la proxima.`);
+          } else {
+            console.log(`Felicidades ganó ${reward * pBetValue} creditos.`);
+          } 
+    } 
+  } 
+let betValue = [1, 5, 10, 20, 25];
+let tragamoneda: ProgressiveSlot = new ProgressiveSlot(2323,betValue,"Egipcio",15,25,4,5,1000);
+
+tragamoneda.playprogressiveSlot(5);
+
--- class/rollerSlot.ts
+++ class/rollerSlot.ts
@@ -1,65 +1,33 @@
 import { Slot } from "./slot";
 
-export class RollerSlot extends Slot{
-    private rollerNumber:number;
-    private well:number;
+export class RollerSlot extends Slot {
+  private well: number;
 
-    public constructor(pId:number,pBetValue:number[],pTheme:string,pSymbolsNumber:number,pWinProbability:number,pRollerNumber:number,pWell:number){
-        super(pId,pBetValue,pTheme,pSymbolsNumber,pWinProbability);
-        this.rollerNumber=pRollerNumber;
-        this.well=pWell;
-    }
-    public getRollerNumber():number{
-        return this.rollerNumber;
-    }
-    public getWell():number{
-        return this.well;
-    }
-    public generateRandomNumber():number[]{
-        let numbers:number[]=new Array(this.rollerNumber);
-        for(let i:number=0;i<this.rollerNumber;i++){
-            numbers[i]=Math.floor(Math.random()*this.getSymbolsNumber()+1);
-        } return numbers;
-    }
-    public getCombination():number{
-        let randomNumber:number[]=this.generateRandomNumber();
-        let combination:number=0;
-            for(let i:number=0;i<randomNumber.length;i++){
-                if(randomNumber[i]===7 && randomNumber.every(e=>randomNumber[0]===e)){
-                    combination=7;
-                } 
-                    else if(i+1<randomNumber.length && randomNumber.slice(i+1).indexOf(randomNumber[i])!==-1){
-                    combination+=1;
-                    }
-            }  console.log(randomNumber);
-        return combination;
-    }
-    public getReward(pBetValue:number):number{
-        let reward=0;
-        let aux=this.getCombination();
-                if(aux===1){
-                    reward=pBetValue*25;
-                    console.log(`Felicidades ganó ${reward} creditos.`);
-                } else if(aux===2){
-                    reward=pBetValue*50;
-                    console.log(`Felicidades ganó ${reward} creditos.`);
-                } else if(aux===3){
-                    reward=pBetValue*100;
-                    console.log(`Felicidades ganó ${reward} creditos.`);
-                } else if(aux>3){
-                    reward=pBetValue*200;
-                    console.log(`Felicidades ganó ${reward} creditos.`);
-                } else if(aux==7){
-                    reward=this.well;
-                    console.log(`¡Felicidades gano el pozo! ${this.well}`);
-                }
-            return reward;
-    }
-}
-let BetValue=[1,2,3,4]
-let newRoller:RollerSlot=new RollerSlot(212,BetValue,'Animal',9,25,3,5000);
+  public constructor(pId:number,pBetValue:number[],pTheme: string,pSymbolsNumber:number,pWinProbability:number,pRollerNumber:number,pWell:number){
+    super(pId,pBetValue,pTheme,pSymbolsNumber,pWinProbability,pRollerNumber);
+    this.rollerNumber=pRollerNumber;
+    this.well=pWell;
+  }
+  public getWell():number{
+    return this.well;
+  }
+  public playRollerSlot(pBetValue:number):number{
+    let reward=0;
+      if(this.verifyBet(pBetValue) && this.checkRollers()){
+          reward=this.getReward();
+      }
+        if(reward===-1){
+            console.log(`Felicidades gano el pozo!!! ${this.well}`);
+        } else if(reward===0){
+            console.log(`Suerte para la proxima.`);
+        } else {
+          console.log(`Felicidades ganó ${reward * pBetValue} creditos.`);
+        } 
+    return reward;
+  } 
+} 
 
+let BetValue = [5,10,15,20];
+let newRoller: RollerSlot = new RollerSlot(212,BetValue,"Animal",9,20,4,5000);
 
-newRoller.getReward(2);
-
-
+newRoller.playRollerSlot(10)
