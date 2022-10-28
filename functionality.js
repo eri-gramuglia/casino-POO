29a840ea8e057fb0dd636d4ed0c2cd61bd30d02a
Arturo Blanco <blancoarturo92@gmail.com>
2022-10-28 18:13:16 -0300

    se trabaja con funcionalidades tragamoneda

--- functionality.js
+++ functionality.js
@@ -1,44 +1,120 @@
 "use strict";
 exports.__esModule = true;
+var main_1 = require("./main");
 var fs = require("fs");
 var readline = require('readline-sync');
+var information = fs.readFileSync('./files.txt/info.txt', 'utf-8');
+var clasificationText = information.split('\\');
+var founds = 100000;
 function welcome() {
-    var welcome = fs.readFileSync('./files.txt/info.txt', 'utf-8');
-    console.log(welcome);
+    var text = clasificationText[0].toString();
+    console.log(text);
     main();
 }
 function main() {
-    console.log('1: JUGAR');
-    var opcion = readline.questionInt();
-    switch (opcion) {
+    console.log('1: MENU');
+    var option = readline.questionInt();
+    switch (option) {
         case 1:
             games();
             break;
         default:
-            throw Error('Ingrese una opcion valida');
+            throw Error('Ingrese una opción valida');
     }
 }
 function games() {
-    console.log('Elija un juego:');
+    console.log('Elija una opción:');
     console.log('1: TRAGAMONEDAS TRADICIONAL');
     console.log('2: TRAGAMONEDAS PROGRESIVO');
     console.log('3: RULETA');
     console.log('4: DADOS');
-    console.log('0: Volver al menu anterior');
-    var opcion = readline.questionInt();
-    callGame(opcion);
+    console.log('0: Volver al inicio');
+    var option = readline.questionInt();
+    callGame(option);
 }
-function callGame(opcion) {
-    switch (opcion) {
+function callGame(option) {
+    switch (option) {
         case 0:
             main();
             break;
         case 1:
+            console.log('1: JUGAR');
+            console.log('2: LEER REGLAS');
+            console.log('0: Volver al menú anterior');
+            var reelOption = readline.questionInt();
+            reelSlot(reelOption);
+            break;
+        case 2:
             console.log('1: ELEGIR APUESTA');
             console.log('2: LEER REGLAS');
-            console.log('0: Volver al menu anterior');
-            var opcion_1 = readline.questionInt();
+            console.log('0: Volver al inicio');
+            var progressiveOption = readline.questionInt();
             break;
+    }
+}
+function reelSlot(option) {
+    switch (option) {
+        case 0:
+            games();
+            break;
+        case 1:
+            var value = readline.questionInt('Ingrese apuesta ( 5 - 10 - 15 - 20 ): ');
+            play(value);
+            subMenu();
+            break;
+        case 2:
+            var text = clasificationText[1].toString();
+            console.log(text);
+            console.log("1: Volver atras:");
+            var backMenu = readline.questionInt();
+            callGame(backMenu);
+            break;
+    }
+}
+function play(value) {
+    var newFounds = main_1.reelSlot1.playReelSlot(value);
+    if (newFounds > 0) {
+        founds += newFounds;
+    }
+    else if (newFounds === 0) {
+        main_1.newCasino.addAmount(value);
+        founds -= value;
+    }
+    else {
+        founds += main_1.reelSlot1.getWell();
+    }
+    console.log("Le quedan ".concat(founds, " creditos."));
+    return newFounds;
+}
+function replay(times, value) {
+    for (var i = 0; i < times; i++) {
+        if (founds >= times * value) {
+            play(value);
+        }
+        else {
+            throw Error('No tiene fondos para esta apuesta.');
+        }
+    }
+    console.log("Le quedan ".concat(founds, " creditos."));
+}
+function subMenu() {
+    console.log('1: JUGAR OTRA VEZ');
+    console.log('2: REPETIR JUGADAS');
+    console.log('3: COBRAR Y SALIR');
+    var gameOption = readline.questionInt();
+    if (gameOption === 1) {
+        reelSlot(1);
+    }
+    else if (gameOption === 2) {
+        var times = readline.questionInt('Ingrese la cantidad de repeticiones:');
+        var value = readline.questionInt('Ingrese apuesta ( 5 - 10 - 15 - 20 ): ');
+        replay(times, value);
+        subMenu();
+    }
+    else if (gameOption === 3) {
+        7;
+        console.log("Se retir\u00F3 con ".concat(founds, " creditos."));
+        main();
     }
 }
 welcome();
--- functionality.ts
+++ functionality.ts
@@ -1,46 +1,114 @@
-import { newCasino } from './main';
+import { newCasino, reelSlot1 } from './main';
 import * as fs from 'fs';
 let readline=require('readline-sync');
+let information:string=fs.readFileSync('./files.txt/info.txt','utf-8');
+let clasificationText:string[]=information.split('\\');
+let founds:number=100000;
 
-    function welcome(){
-        let welcome:string=fs.readFileSync('./files.txt/info.txt','utf-8');
-        console.log(welcome);
+    function welcome():void{
+        let text:string=clasificationText[0].toString()
+        console.log(text);
         main();
     }
     function main():void{
-        console.log('1: JUGAR');
-        let opcion=readline.questionInt();
-        switch(opcion){
+        console.log('1: MENU');
+        let option:number=readline.questionInt();
+        switch(option){
             case 1:
                 games();
                 break;
             default:
-            throw Error('Ingrese una opcion valida');
+            throw Error('Ingrese una opción valida');
         }
     }   
-    function games(){
-        console.log('Elija un juego:');
+    function games():void{
+        console.log('Elija una opción:');
         console.log('1: TRAGAMONEDAS TRADICIONAL');
         console.log('2: TRAGAMONEDAS PROGRESIVO');
         console.log('3: RULETA');
         console.log('4: DADOS');
-        console.log('0: Volver al menu anterior');
-        let opcion=readline.questionInt();
-        callGame(opcion);
+        console.log('0: Volver al inicio');
+        let option:number=readline.questionInt();
+        callGame(option);
     }
-    function callGame(opcion:number){
-        switch(opcion){
+    function callGame(option:number):void{
+        switch(option){
             case 0:
             main();
             break;
             case 1:
+            console.log('1: JUGAR');
+            console.log('2: LEER REGLAS');
+            console.log('0: Volver al menú anterior');
+            let reelOption:number=readline.questionInt();
+            reelSlot(reelOption);
+            break; 
+            case 2:
             console.log('1: ELEGIR APUESTA');
             console.log('2: LEER REGLAS');
-            console.log('0: Volver al menu anterior');
-            let opcion=readline.questionInt();
+            console.log('0: Volver al inicio');
+            let progressiveOption:number=readline.questionInt();
+        break;
+    }
+}
+    function reelSlot(option:number):void{
+        switch(option){
+            case 0:
+            games();
+            break;
+            case 1:
+            let value:number=readline.questionInt('Ingrese apuesta ( 5 - 10 - 15 - 20 ): ')
+            play(value);
+            subMenu();
+            break;
+            case 2:
+            let text:string=clasificationText[1].toString();
+            console.log(text);
+            console.log(`1: Volver atras:`);
+            let backMenu:number=readline.questionInt();
+            callGame(backMenu);
             break;
         }
     }
-
+    function play(value:number):number{
+        let newFounds=reelSlot1.playReelSlot(value);
+        if(newFounds>0){
+            founds+=newFounds;
+        } else if(newFounds===0){
+            newCasino.addAmount(value);
+            founds-=value;
+        } else {
+            founds+=reelSlot1.getWell();
+        } 
+        console.log(`Le quedan ${founds} creditos.`);
+        return newFounds;
+    }         
+    
+    function replay(times:number,value:number):void{
+        for(let i:number=0;i<times;i++){
+            if(founds>=times*value){
+            play(value);
+            } else{
+            throw Error('No tiene fondos para esta apuesta.')
+            }
+        }   console.log(`Le quedan ${founds} creditos.`);
+    }
+    function subMenu():void{
+        console.log('1: JUGAR OTRA VEZ');
+        console.log('2: REPETIR JUGADAS');
+        console.log('3: COBRAR Y SALIR');
+        let gameOption:number=readline.questionInt();
+        if(gameOption===1){
+            reelSlot(1);
+        } else if(gameOption===2){
+            let times:number=readline.questionInt('Ingrese la cantidad de repeticiones:');
+            let value:number=readline.questionInt('Ingrese apuesta ( 5 - 10 - 15 - 20 ): ')
+            replay(times,value);
+            subMenu();
+        } else if(gameOption===3){7
+            console.log(`Se retiró con ${founds} creditos.`);
+            main();
+        }
+    }
 welcome();
 
