7b9d6a1631f2c6a0e936bc4e019603bfa9e19a75
Your Name <dgj7785@gmail.com>
2022-10-31 16:00:02 -0300

    Upgrade_Class_TurningTurn_v3

--- class/TurningTurn.ts
+++ class/TurningTurn.ts
@@ -1,7 +1,6 @@
 import { Roulette } from "./roulette";
 import { Player } from "./player";
 
-
 export class TurningTurn {
     private _turnNumber: number;
     private _roulette: Roulette;
@@ -85,20 +84,20 @@ export class TurningTurn {
                     this._player.setFoundsAvailable(this._player.getFoundsAvailable()+(this.getBetValue()*35));
                     
                 }else {
-                    console.log ("Lost")
+                    console.log ("Lost PLENO")
                 }
-                if (this._colorSelect != " "){
+                if (this._colorSelect !=" "){
                     if (this._colorSelect === this._roulette.getColor(aux)){
                         console.log ("----------------------------------------------------------------");
-                        console.log ("WINS COLOR")
+                        console.log (`WINS Color: ${this._colorSelect}`)
                         console.log ("----------------------------------------------------------------");
                         this._player.setFoundsAvailable(this._player.getFoundsAvailable()+(this.getBetValue()*1));
                     }
                 }
-                if (this._evenOroddSelect != " "){
+                if (this._evenOroddSelect !=" "){
                     if (this._evenOroddSelect === this._roulette.getEvenOrOdd(aux)){
                         console.log ("----------------------------------------------------------------");
-                        console.log ("WINS")
+                        console.log (`WINS ${this._evenOroddSelect}`)
                         console.log ("----------------------------------------------------------------");
                         this._player.setFoundsAvailable(this._player.getFoundsAvailable()+(this.getBetValue()*1))
                     }
@@ -106,7 +105,7 @@ export class TurningTurn {
                 if (this._dozenSelect != " "){
                     if (this._dozenSelect === this._roulette.getDozen(aux)){
                         console.log ("----------------------------------------------------------------");
-                        console.log ("WINS")
+                        console.log (`WINS ${this._dozenSelect}`)
                         console.log ("----------------------------------------------------------------");
                         this._player.setFoundsAvailable(this._player.getFoundsAvailable()+(this.getBetValue()*2))
                     }
@@ -114,7 +113,7 @@ export class TurningTurn {
                 if (this._highOrLowSelect != " "){
                     if (this._highOrLowSelect === this._roulette.getHighOrLow(aux)){
                         console.log ("----------------------------------------------------------------");
-                        console.log ("WINS")
+                        console.log (`WINS ${this._highOrLowSelect}`)
                         console.log ("----------------------------------------------------------------");
                         this._player.setFoundsAvailable(this._player.getFoundsAvailable()+(this.getBetValue()*1))
                     }
@@ -129,10 +128,11 @@ let playerOne: Player = new Player (1,"Daniel","Jerez",10000);
 
 let red: number[] = new Array (1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37)
 let black: number[] = new Array (2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38)
+
 let rouletteOne : Roulette = new Roulette(1,red,black,2,500000)
 
 // instance TurningTurn
 
-let TurningTurnOne : TurningTurn = new TurningTurn(1,rouletteOne,playerOne,100,25,"ROJO")
+let TurningTurnOne : TurningTurn = new TurningTurn(1,rouletteOne,playerOne,100,25,"ROJO","PAR","1ra Docena","Numero BAJO")
 
 TurningTurnOne.turning()
