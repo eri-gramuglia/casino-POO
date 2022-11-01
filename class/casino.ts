25270b2ba7086d26fdcee97e57600b1a681b2fff
Arturo Blanco <blancoarturo92@gmail.com>
2022-11-01 17:09:44 -0300

    se corrige ortografia

--- class/casino.ts
+++ class/casino.ts
@@ -4,16 +4,16 @@ export class Casino {
     private casinoName:string;
     private progressiveSlotList:ProgressiveSlot[];
     private reelSlotList:ReelSlot[];
-    //private roulleteList:Roullete[];
+    //private rouletteList:Roulette[];
     //private crapsList:Craps[];
     private treasury:number;
 
-    public constructor(pName:string,pProgressiveSlotList:ProgressiveSlot[],pReelSlotList:ReelSlot[],/*pRoulleteList:Roullete[],pCrapsList:Craps[],*/pTreasury:number){
+    public constructor(pName:string,pProgressiveSlotList:ProgressiveSlot[],pReelSlotList:ReelSlot[],/*pRouletteList:Roulette[],pCrapsList:Craps[],*/pTreasury:number){
         this.casinoName=pName;
         this.progressiveSlotList=pProgressiveSlotList;
         this.reelSlotList=pReelSlotList;
         this.treasury=pTreasury;
-        //this.roulleteList=pRoulleteList;
+        //this.rouletteList=pRouletteList;
         //this.crapsList=pCrapsList;
     }
     public getCasinoName():string{
@@ -48,10 +48,10 @@ export class Casino {
                     throw Error(`No existe esta maquina en el casino`);
                 } 
     }
-    /*public getRoullete(id:number):boolean{
+    /*public getRoulette(id:number):boolean{
         let aux=false;
-            for(let i=0;i<this.roulleteList.length;i++){
-                if(id===this.roullete[i].getId()){
+            for(let i=0;i<this.rouletteList.length;i++){
+                if(id===this.roulette[i].getId()){
                     aux=true;
                 } 
             } if(aux){
