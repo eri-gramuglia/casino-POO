7c664635a096d155e39085937fa070520d6160e5
arturo-blanco <arturoblanco@tdf.com>
2022-11-07 19:39:37 -0300

    upgrade roulette

--- class/roulette.js
+++ class/roulette.js
@@ -2,15 +2,11 @@
 exports.__esModule = true;
 exports.Roulette = void 0;
 var Roulette = /** @class */ (function () {
-    //   private _boxFound: number;
-    //   private _player: Player;
-    function Roulette(p_id, p_numRed, /*p_numBlack: Array<number>,*/ p_numRandom) {
+    function Roulette(p_id, p_numberSelect, p_betValue, p_betOption) {
         this._id = p_id;
-        this._numRed = p_numRed;
-        //this._numBlack = p_numBlack;
-        this._numRandom = p_numRandom;
-        //        this._boxFound = p_boxFound;
-        //        this._player = p_player;
+        this._numberSelected = p_numberSelect;
+        this._betValue = p_betValue;
+        this._betOption = p_betOption;
     }
     Roulette.prototype.setId = function (p_id) {
         this._id = p_id;
@@ -18,47 +14,23 @@ var Roulette = /** @class */ (function () {
     Roulette.prototype.getId = function () {
         return this._id;
     };
-    Roulette.prototype.setNumRed = function (p_numRed) {
-        this._numRed = p_numRed;
+    Roulette.prototype.setNumSelected = function (p_numSelect) {
+        this._numberSelected = p_numSelect;
     };
-    Roulette.prototype.getNumRed = function () {
-        return this._numRed;
+    Roulette.prototype.getNumSelected = function () {
+        return this._numberSelected;
     };
-    /*     public setNumWhite(p_numBlack:Array<number>):void {
-            this._numBlack = p_numBlack;
-        }
-        public getNumWhite():Array<number> {
-            return this._numBlack;
-        } */
-    /*    public setBoxFound(p_boxFound:number):void {
-            this._boxFound = p_boxFound;
-        }
-        public getBoxFound():number {
-            return this._boxFound;
-        }
-        public setPlayersList(p_player: Player):void {
-            this._player = p_player;
-        }*/
-    Roulette.prototype.getNumRandom = function () {
-        var min = Math.ceil(0);
-        var max = Math.floor(36);
-        this._numRandom = Math.floor(Math.random() * (max - min + 1) + min);
-        return this._numRandom;
+    Roulette.prototype.setBetValue = function (p_betValue) {
+        this._betValue = p_betValue;
     };
-    Roulette.prototype.getColor = function (p_number) {
-        //let aux: string = " ";
-        //console.log (this._numRed.length)
-        if (p_number === 0) {
-            return "VERDE";
-        }
-        else {
-            for (var i = 0; i <= this._numRed.length; i++) {
-                if (p_number === this._numRed[i]) {
-                    return "ROJO";
-                }
-            }
-            return "NEGRO";
-        }
+    Roulette.prototype.getBetValue = function () {
+        return this._betValue;
+    };
+    Roulette.prototype.setBetOption = function (p_betOption) {
+        this._betOption = p_betOption;
+    };
+    Roulette.prototype.getBetOption = function () {
+        return this._betOption;
     };
     Roulette.prototype.getEvenOrOdd = function (p_number) {
         if (p_number % 2 === 0) {
@@ -67,6 +39,25 @@ var Roulette = /** @class */ (function () {
         else {
             return "IMPAR";
         }
+    };
+    Roulette.prototype.getColor = function (p_number) {
+        var auxColor;
+        if (p_number === 0) {
+            return auxColor = "VERDE";
+        }
+        else if (p_number <= 9 && p_number % 2 !== 0) {
+            return auxColor = "ROJO";
+        }
+        else if (p_number <= 18 && p_number % 2 === 0 && p_number !== 10) {
+            return auxColor = "ROJO";
+        }
+        else if (p_number <= 27 && p_number % 2 !== 0) {
+            return auxColor = "ROJO";
+        }
+        else if (p_number % 2 === 0 && p_number !== 28) {
+            return auxColor = "ROJO";
+        }
+        return auxColor = "NEGRO";
     };
     Roulette.prototype.getDozen = function (p_number) {
         if (p_number <= 12) {
@@ -86,6 +77,57 @@ var Roulette = /** @class */ (function () {
         else {
             return "Numero BAJO";
         }
+    };
+    Roulette.prototype.toTurn = function () {
+        var min = Math.ceil(0);
+        var max = Math.floor(36);
+        var numRandom = Math.floor(Math.random() * (max - min + 1) + min);
+        var optionResult = ["number", this.getColor(numRandom), this.getEvenOrOdd(numRandom), this.getDozen(numRandom), this.getHighOrLow(numRandom)];
+        console.log("----------------------------------------------------------------");
+        console.log("Girando...y Sale: ".concat(numRandom, " el ").concat(this.getColor(numRandom)));
+        console.log("----------------------------------------------------------------");
+        if (numRandom === 0) {
+            for (var i_1 = 0; i_1 < this._betValue.length; i_1++) {
+                this._betValue[i_1] = -this._betValue[i_1];
+            }
+            console.log("La Casa GANA la MESA");
+        }
+        else {
+            if (numRandom === this._numberSelected) {
+                console.log("----------------------------------------------------------------");
+                console.log("GANASTE el PLENO, tu Apusta se Multiplica x35");
+                console.log("----------------------------------------------------------------");
+                this._betValue[0] += this._betValue[0] * 35;
+            }
+            else if (this._numberSelected !== -1) {
+                console.log("----------------------------------------------------------------");
+                console.log("perdiste lo apostado para PLENO");
+                console.log("----------------------------------------------------------------");
+                this._betValue[0] = -this._betValue[0];
+            }
+            for (var i = 1; i < this._betValue.length; i++) {
+                if (this._betValue[i] !== 0) {
+                    if (this._betOption[i] === optionResult[i]) {
+                        console.log("----------------------------------------------------------------");
+                        console.log("GANASTE lo Aposta a: ".concat(this._betOption[i]));
+                        console.log("----------------------------------------------------------------");
+                        if (i === 3) {
+                            this._betValue[i] += this._betValue[i] * 2;
+                        }
+                        else {
+                            this._betValue[i] += this._betValue[i];
+                        }
+                    }
+                    else {
+                        console.log("----------------------------------------------------------------");
+                        console.log("Perdiste lo Aposta a: ".concat(this._betOption[i]));
+                        console.log("----------------------------------------------------------------");
+                        this._betValue[i] = -this._betValue[i];
+                    }
+                }
+            }
+        }
+        return this._betValue;
     };
     return Roulette;
 }());
